8ca9f6f8a880d151e2f68c1d3ac051f55da2573b
Nicolas Brassard <nico@nitriques.com>
2014-10-24 17:53:08 -0400

    save content page

--- content/content.save.php
+++ content/content.save.php
@@ -16,6 +16,56 @@
 		 * Builds the content view
 		 */
 		public function view() {
+			if (!is_array($this->_context) || empty($this->_context)) {
+				$this->_Result['error'] = 'Parameters not found';
+				return;
+			}
 			
+			$entriesId = explode(',', MySQL::cleanValue($this->_context[0]));
+			$entriesId = array_map(intval, $entriesId);
+			if (!is_array($entriesId) || empty($entriesId)) {
+				$this->_Result['error'] = 'No entry no found';
+				return;
+			}
+			
+			$parentFieldId = intval(MySQL::cleanValue($this->_context[1]));
+			if ($parentFieldId < 1) {
+				$this->_Result['error'] = 'Parent id not valid';
+				return;
+			}
+			
+			$parentField = FieldManager::fetch($parentFieldId);
+			if (!$parentField || empty($parentField)) {
+				$this->_Result['error'] = 'Parent field not found';
+				return;
+			}
+			
+			$entryId = intval(MySQL::cleanValue($this->_context[2]));
+			if ($entryId < 1) {
+				$this->_Result['error'] = 'Parent entry id not valid';
+				return;
+			}
+			
+			$entry = EntryManager::fetch($entryId);
+			if ($entry == null || count($entry) != 1) {
+				$this->_Result['error'] = 'Parent entry not found';
+				return;
+			}
+			if (is_array($entry)) {
+				$entry = $entry[0];
+			}
+			$entryData = $entry->getData();
+			
+			// save the new data
+			$entryData[$parentFieldId]['entries'] = implode(',', $entriesId);
+			$entry->setData($parentFieldId, $entryData[$parentFieldId]);
+			if (!$entry->commit()) {
+				$this->_Result['error'] = 'Could not save entry';
+				return;
+			}
+			
+			$this->_Result['entry-id'] = $entryId;
+			$this->_Result['ok'] = true;
+			$this->_Result['entries'] = $entryData[$parentFieldId];
 		}
 	}

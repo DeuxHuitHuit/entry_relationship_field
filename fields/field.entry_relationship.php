DeuxHuitHuit/entry_relationship_field

commit 2a08660f0fe963e11c10d7efc566f8e75919b38c

Added type guards into processRawFieldData
We need to make sure to check what is the data type of the $data
argument because it may vary.

Fixes #31

fields/field.entry_relationship.php
@@ -177,8 +177,18 @@
 		 */
 		public function processRawFieldData($data, &$status, &$message = null, $simulate = false, $entry_id = null) {
 			$status = self::__OK__;
+			$entries = null;
 			
-			$entries = $data['entries'];
+			if (!is_array($data) || !is_string($data)) {
+				return null;
+			}
+			
+			if (isset($data['entries'])) {
+				$entries = $data['entries'];
+			}
+			else if (is_string($data)) {
+				$entries = $data;
+			}
 			
 			$row = array(
 				'entries' => $entries

commit 02ac32696e5f55dcb9743daffc2009d7b5f3b4ea

Value cleared on save fixed
This bug was introduced in 2a08660. Return null only if the $data variable is not an array NOR a string.

cc @PascalPiche
Re #31

fields/field.entry_relationship.php
@@ -188,7 +188,7 @@
 			$status = self::__OK__;
 			$entries = null;
 			
-			if (!is_array($data) || !is_string($data)) {
+			if (!is_array($data) && !is_string($data)) {
 				return null;
 			}
 			
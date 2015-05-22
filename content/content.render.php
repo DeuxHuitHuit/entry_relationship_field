eace10ef7e5e51ee4df017b5d9ade7806e789e62
Nicolas Brassard <nico@nitriques.com>
2015-05-22 16:57:03 -0400

    Fix XSL path issue on Windows
    This change make sure we replace backslashes \ with forwardslashes / which fixes issues with the XSLT parser.
    
    Fixes #44
    
    Re: https://github.com/symphonycms/symphony-2/issues/2316

--- content/content.render.php
+++ content/content.render.php
@@ -176,7 +176,7 @@
 						$xmlString = $xmlData->generate($indent, 0);
 						$xsl = '<?xml version="1.0" encoding="UTF-8"?>
 						<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-							<xsl:import href="' . $xslFilePath . '"/>
+							<xsl:import href="' . str_replace('\\', '/',  $xslFilePath) . '"/>
 							<xsl:output method="xml" omit-xml-declaration="yes" encoding="UTF-8" indent="no" />
 							<xsl:template match="/">
 								<xsl:apply-templates select="/data" ' . $xmlMode . ' />

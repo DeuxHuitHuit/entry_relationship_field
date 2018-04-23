69e8e26e2a857ded86d538d30c6e51fc8fd41ec6
hotdoy <rouleau.vincent@gmail.com>
2018-04-23 13:11:09 -0400

    Add french localisation (#76)
    * Added french localisation  121e890
    * fixed typo 202e92c

---
+++ lang/lang.fr.php
@@ -0,0 +1,93 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name' => 'Deux Huit Huit',
+			'email' => 'open-source (at) deuxhuithuit (dot) com',
+			'website' => 'http://www.deuxhuithuit.com'
+		),
+		'release-date' => '2018-04-20'
+	);
+	
+	
+	/*
+	 * EXTENSION: Client Logo
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Client logo path (relative to workspace)' => 
+		'Chemin d\accès du logo (chemin relatif au dossier workspace)',
+
+		'System' => 
+		'Système',
+
+		'ERF Clean up' => 
+		'Nettoyage relationnel',
+
+		'Entry Relationship' => 
+		'Relation',
+
+		'At least one section must be chosen' => 
+		'Au moins une entrée doit être choisi',
+
+		'Find entries that links to the specified filter' => 
+		'Trouve les entrées lié au filtre spécifié',
+
+		'None' => 
+		'Aucun',
+
+		'Error: entry `%s` not found' => 
+		'Erreur: Entrée `%s` introuvable',
+
+		'Available sections %s' => 
+		'Sections disponible %s',
+
+		'Search for entries' => 
+		'Chercher un entrée',
+
+		'Related section: ' =>
+		'Section liée',
+
+		'Create new' => 
+		'Créer une entrée', 
+
+		'Link to entry' => 
+		'Lier une entrée', 
+
+		'Un-link' => 
+		'Délier', 
+
+		'Replace' => 
+		'Remplacer', 
+
+		'Close' => 
+		'Fermer', 
+
+		'Please choose what to delete' => 
+		'Selection des entrées à supprimer', 
+
+		'List of all orphans entries' => 
+		'Liste des entrées orphelines', 
+
+
+		'Entry Relationship Clean up' => 
+		'Nettoyage relationnel',
+
+		'Fields linked to' => 
+		'Champs liés', 
+
+		'Number of entries' => 
+		'Entrées total', 
+
+		'Orphan entries' => 
+		'Entrées orphelines', 
+
+		'Linked entries' => 
+		'Entrées liées', 
+
+	);
+	
+	
